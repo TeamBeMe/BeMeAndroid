aea71a17dd956c2ca375e87be097c06b2323e34c
yjooooo <thsduswn4141@naver.com>
2020-12-30 02:36:03 +0900

    #24 [STYLE] : Make Explore Fragment UI

--- app/src/main/java/com/teambeme/beme/explore/view/ExploreFragment.kt
+++ app/src/main/java/com/teambeme/beme/explore/view/ExploreFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.teambeme.beme.R
+import com.teambeme.beme.explore.viewmodel.ExploreViewModel
 
 class ExploreFragment : Fragment() {
+    private val exploreViewModel: ExploreViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
