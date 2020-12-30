36b383d17e45ec7bbbb7e3d8ced52ee060b5e81e
Hyun Woo Lee <l2hyunwoo@gmail.com>
2020-12-30 21:04:15 +0900

    #49 [FEAT] : Connect viewpager and adapter

--- app/src/main/java/com/teambeme/beme/home/view/HomeFragment.kt
+++ app/src/main/java/com/teambeme/beme/home/view/HomeFragment.kt
@@ -1,19 +1,25 @@
 package com.teambeme.beme.home.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.teambeme.beme.R
+import com.teambeme.beme.databinding.FragmentHomeBinding
+import com.teambeme.beme.home.adapter.QuestionPagerAdapter
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        val questionPagerAdapter = QuestionPagerAdapter()
+        binding.vpHomeQuestionSlider.adapter = questionPagerAdapter
+        return binding.root
     }
 }
