ddcf40f9c443851e32f1e25a7a49f2d4f90083f7
ub1n <bin951024@naver.com>
2020-12-30 19:09:55 +0900

    #36 [FEAT] : Make Adapter base setting

--- app/src/main/java/com/teambeme/beme/reply/view/ReplyActivity.kt
+++ app/src/main/java/com/teambeme/beme/reply/view/ReplyActivity.kt
@@ -3,10 +3,17 @@ package com.teambeme.beme.reply.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.teambeme.beme.R
+import com.teambeme.beme.base.BindingActivity
+import com.teambeme.beme.databinding.ActivityReplyBinding
+import com.teambeme.beme.reply.adapter.ReplyParentAdapter
+import com.teambeme.beme.reply.model.initReplyList
 
-class ReplyActivity : AppCompatActivity() {
+class ReplyActivity : BindingActivity<ActivityReplyBinding>(R.layout.activity_reply) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reply)
+        val replyAdapter=ReplyParentAdapter()
+        binding.rcvReplyParent.adapter=replyAdapter
+
+
     }
 }
