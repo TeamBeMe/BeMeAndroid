de36e362db41d1fae6e566a0612e265ab703d931
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-01-01 21:53:38 +0900

    #53 [FEAT] : BindingAdapters에 EditText, ImageView 함수 추가

--- app/src/main/java/com/teambeme/beme/util/BindingAdapters.kt
+++ app/src/main/java/com/teambeme/beme/util/BindingAdapters.kt
@@ -1,8 +1,15 @@
 package com.teambeme.beme.util
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.teambeme.beme.explore.adapter.OthermindsRcvAdapter
 import com.teambeme.beme.explore.adapter.OtherquestionsRcvAdapter
 import com.teambeme.beme.explore.model.OthermindsData
@@ -21,7 +28,10 @@ object BindingAdapters {
 
     @BindingAdapter("setOtherquestions")
     @JvmStatic
-    fun setOtherquestions(recyclerView: RecyclerView, otherquestions: MutableList<OtherquestionsData>?) {
+    fun setOtherquestions(
+        recyclerView: RecyclerView,
+        otherquestions: MutableList<OtherquestionsData>?
+    ) {
         if (recyclerView.adapter != null) with(recyclerView.adapter as OtherquestionsRcvAdapter) {
             otherquestions?.let {
                 submitList(otherquestions)
@@ -39,5 +49,38 @@ object BindingAdapters {
     @JvmStatic
     fun setTime(textView: TextView, time: String) {
         textView.text = time + "분 전"
+    }
+
+    @BindingAdapter("app:setSrcFromUrl")
+    @JvmStatic
+    fun setSrcFromUrl(imageView: ImageView, url: String) {
+        Glide.with(imageView.context)
+            .load(url)
+            .centerCrop()
+            .into(imageView)
+    }
+
+    @InverseBindingAdapter(attribute = "android:text", event = "textAttrChanged")
+    @JvmStatic
+    fun getEditTextString(view: EditText): String = view.text.toString()
+
+    @BindingAdapter("android:text")
+    @JvmStatic
+    fun setEditTextString(view: EditText, text: String) {
+        if (view.text.toString() != text) view.setText(text)
+    }
+
+    @BindingAdapter("textAttrChanged")
+    @JvmStatic
+    fun setEditTextTextWatcher(view: EditText, textAttrChanged: InverseBindingListener) {
+        view.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                textAttrChanged.onChange()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
     }
 }
