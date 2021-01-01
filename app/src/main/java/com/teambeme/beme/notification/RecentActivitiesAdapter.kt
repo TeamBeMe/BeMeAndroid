8560944265c0b3ca069eab491d01b46385a3ccfa
sem <uhoo1216@gmail.com>
2021-01-01 21:11:14 +0900

    rcv_recent_activities에 해당하는 Adapter 생성

---
+++ app/src/main/java/com/teambeme/beme/notification/RecentActivitiesAdapter.kt
@@ -0,0 +1,26 @@
+package com.teambeme.beme.notification
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.teambeme.beme.databinding.ItemRecentActivitiesBinding
+
+
+class RecentActivitiesAdapter (private val context: Context) :
+    RecyclerView.Adapter<RecentActivitiesViewHolder>(){
+
+    var recentActivitiesList = mutableListOf<RecentActivitiesData>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecentActivitiesViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemRecentActivitiesBinding.inflate(inflater, parent, false)
+        return RecentActivitiesViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = recentActivitiesList.size
+
+    override fun onBindViewHolder(holder:  RecentActivitiesViewHolder, position: Int) {
+        holder.bind(recentActivitiesList[position])
+    }
+}
