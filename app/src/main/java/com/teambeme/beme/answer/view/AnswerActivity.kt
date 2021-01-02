0c48caa22baa4c1a23b396bc6fdcc03bbbce5b0e
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-01-02 18:49:42 +0900

    #103 [CHORE] : Answer flow file packaging

---
+++ app/src/main/java/com/teambeme/beme/answer/view/AnswerActivity.kt
@@ -0,0 +1,12 @@
+package com.teambeme.beme.answer.view
+
+import android.os.Bundle
+import com.teambeme.beme.R
+import com.teambeme.beme.base.BindingActivity
+import com.teambeme.beme.databinding.ActivityAnswerBinding
+
+class AnswerActivity : BindingActivity<ActivityAnswerBinding>(R.layout.activity_answer) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/teambeme/beme/answer/viewmodel/AnswerViewModel.kt
@@ -0,0 +1,6 @@
+package com.teambeme.beme.answer.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class AnswerViewModel : ViewModel() {
+}
