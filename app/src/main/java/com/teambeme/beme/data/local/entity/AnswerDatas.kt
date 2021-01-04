3a92834b9ac2a1b3cf3a4e9af0f0c97f2fe02c09
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-01-04 17:52:11 +0900

    #116 [FEAT] : Add Answer Data Entity

---
+++ app/src/main/java/com/teambeme/beme/data/local/entity/AnswerDatas.kt
@@ -0,0 +1,13 @@
+package com.teambeme.beme.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "answer_record_table")
+data class AnswerDatas(
+    @PrimaryKey
+    val questionId: Long,
+    @ColumnInfo(name = "answer")
+    val answer: String
+)
