e7195e297cad44d55f2fd8026a6b1b97ff097e1a
yjooooo <thsduswn4141@naver.com>
2021-01-04 18:36:20 +0900

    #119 [STYLE] : Add NestedScrollView to ExploreFragment & Add nestedScrollingEnabled=false to otherquestions Recyclerview

--- app/src/main/java/com/teambeme/beme/explore/viewmodel/ExploreViewModel.kt
+++ app/src/main/java/com/teambeme/beme/explore/viewmodel/ExploreViewModel.kt
@@ -92,9 +92,36 @@ class ExploreViewModel : ViewModel() {
                 title = "올해 1월과 가장 달라진 점은 무엇인가요?",
                 content = "나이, 나이, 나이, 나이, 나이, 나이, 나이",
                 time = "5"
+            ),
+            OtherquestionsData(
+                userId = "7",
+                category = "일상",
+                title = "과거, 미래 둘 중 하나로 시간 여행을 할 수 있다면 무엇을 선택할 것인가요?",
+                content = "과거, 미래, 현재, 과거, 미래, 현재, 과거, 미래, 현재, 과거, 미래, 현재",
+                time = "15"
+            ),
+            OtherquestionsData(
+                userId = "8",
+                category = "이야기",
+                title = "올해 1월과 가장 달라진 점은 무엇인가요?",
+                content = "나이, 나이, 나이, 나이, 나이, 나이, 나이",
+                time = "3"
+            ),
+            OtherquestionsData(
+                userId = "9",
+                category = "미래",
+                title = "과거, 미래 둘 중 하나로 시간 여행을 할 수 있다면 무엇을 선택할 것인가요?",
+                content = "과거, 미래, 현재, 과거, 미래, 현재, 과거, 미래, 현재, 과거, 미래, 현재",
+                time = "4"
+            ),
+            OtherquestionsData(
+                userId = "10",
+                category = "의미",
+                title = "올해 1월과 가장 달라진 점은 무엇인가요?",
+                content = "나이, 나이, 나이, 나이, 나이, 나이, 나이",
+                time = "5"
             )
         )
         _otherquestionsList.value = dummyOtherquestionsList.toMutableList()
     }
-
 }
