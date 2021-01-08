923866fc24db5c6ec9b0dec1f37434202d3fc036
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-01-08 18:37:06 +0900

    #149 [STYLE] : Bottomsheet Design

---
+++ app/src/main/java/com/teambeme/beme/home/view/InfoChangeFragment.kt
@@ -0,0 +1,19 @@
+package com.teambeme.beme.home.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.teambeme.beme.R
+
+class InfoChangeFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info_change, container, false)
+    }
+}
