6b1591ecf2c288686e14c20c5e62cc98a73e2d7e
sem <uhoo1216@gmail.com>
2021-01-08 16:45:46 +0900

    [STYLE] : 기존 Notification 뷰에서 title과 rcv 사이 간격 벌려 놓음

---
+++ app/src/main/java/com/teambeme/beme/idsearch/FollowingAfterIdSearch.kt
@@ -0,0 +1,12 @@
+package com.teambeme.beme.idsearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teambeme.beme.R
+
+class FollowingAfterIdSearch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_following_after_id_search)
+    }
+}
