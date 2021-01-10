f80f35bb0ec228995be80c8da83c324954b5bb98
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-01-10 19:11:28 +0900

    #216 [FIX] : Login server 연동

--- app/src/main/java/com/teambeme/beme/login/repository/LoginRepository.kt
+++ app/src/main/java/com/teambeme/beme/login/repository/LoginRepository.kt
@@ -1,7 +1,8 @@
 package com.teambeme.beme.login.repository
 
 import com.teambeme.beme.login.model.ResponseLogin
+import retrofit2.Call
 
 interface LoginRepository {
-    fun login(nickname: String, password: String): ResponseLogin?
+    fun login(nickname: String, password: String): Call<ResponseLogin>
 }
