c444c0cb38d6f2e8af20e887c6cdb51c1d54228f
ub1n <bin951024@naver.com>
2021-01-14 08:02:45 +0900

    issue track: #311 [FEAT] My Page Server connect #312 [FEAT] Detail Page Server Connect

--- app/src/main/java/com/teambeme/beme/mypage/model/ResponseProfile.kt
+++ app/src/main/java/com/teambeme/beme/mypage/model/ResponseProfile.kt
@@ -1,16 +1,25 @@
 package com.teambeme.beme.mypage.model
 
-
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class ResponseProfile(
+    @SerializedName("data")
+    val `data`: Data,
     @SerializedName("message")
     val message: String,
     @SerializedName("status")
     val status: Int,
     @SerializedName("success")
     val success: Boolean
-) : Parcelable
+) : Parcelable {
+    @Parcelize
+    data class Data(
+        @SerializedName("image")
+        val image: String?,
+        @SerializedName("user_id")
+        val userId: Int
+    ) : Parcelable
+}
