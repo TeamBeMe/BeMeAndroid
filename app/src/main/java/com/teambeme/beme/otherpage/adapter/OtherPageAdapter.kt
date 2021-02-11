0bf610da8ac4a319cbdf5e0e851aa40cf34f9a82
ub1n <bin951024@naver.com>
2021-02-11 13:39:09 +0900

    #519 [FIX] : OtherPage 답하지 않은 글 보는것 막기

--- app/src/main/java/com/teambeme/beme/otherpage/adapter/OtherPageAdapter.kt
+++ app/src/main/java/com/teambeme/beme/otherpage/adapter/OtherPageAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -35,20 +36,33 @@ class OtherPageAdapter(private val otherViewModel: OtherPageViewModel) :
     override fun onBindViewHolder(holder: OtherPageViewHolder, position: Int) {
         holder.bind(getItem(position))
         holder.itemView.setOnClickListener { view ->
-            val intent = Intent(view.context, DetailActivity::class.java)
-            intent.putExtra("answerId", getItem(position).id)
-            view.context.startActivity(intent)
+            if (getItem(position).isAnswered) {
+                val intent = Intent(view.context, DetailActivity::class.java)
+                intent.putExtra("answerId", getItem(position).id)
+                view.context.startActivity(intent)
+            } else {
+                Toast.makeText(view.context, "팔로잉 탭에서 질문에 답변을 하면 볼 수 있어요", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
         holder.bind(getItem(position)).let {
             with(holder) {
-                scrap.setOnClickListener {
-                    otherViewModel.setPosition(position)
-                    if (getItem(position).isScrapped) {
-                        getItem(position).isScrapped = false
-                        scrap.setImageResource(R.drawable.ic_scrap_off_mypage)
+                scrap.setOnClickListener { view ->
+                    if (getItem(position).isAnswered) {
+                        otherViewModel.setPosition(position)
+                        if (getItem(position).isScrapped) {
+                            getItem(position).isScrapped = false
+                            scrap.setImageResource(R.drawable.ic_scrap_off_mypage)
+                        } else {
+                            getItem(position).isScrapped = true
+                            scrap.setImageResource(R.drawable.ic_scrap_on_mypage)
+                        }
                     } else {
-                        getItem(position).isScrapped = true
-                        scrap.setImageResource(R.drawable.ic_scrap_on_mypage)
+                        Toast.makeText(
+                            view.context,
+                            "팔로잉 탭에서 질문에 답변을 하면 스크랩 할 수 있어요",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
             }
