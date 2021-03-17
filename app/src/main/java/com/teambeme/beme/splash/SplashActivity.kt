e982dee670d17d48594988ad0224351f8080656f
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-17 15:22:52 +0900

    #607 [FEAT] : AppUpdateManager 구현

--- app/src/main/java/com/teambeme/beme/splash/SplashActivity.kt
+++ app/src/main/java/com/teambeme/beme/splash/SplashActivity.kt
@@ -1,20 +1,31 @@
 package com.teambeme.beme.splash
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.view.WindowInsets.*
 import android.view.WindowInsetsController
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallState
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.teambeme.beme.R
+import com.teambeme.beme.base.BindingActivity
 import com.teambeme.beme.data.local.singleton.BeMeAuthPreference
 import com.teambeme.beme.data.remote.datasource.LoginDataSourceImpl
 import com.teambeme.beme.data.remote.singleton.RetrofitObjects
+import com.teambeme.beme.databinding.ActivitySplashBinding
 import com.teambeme.beme.login.model.ResponseLogin
 import com.teambeme.beme.login.repository.LoginRepositoryImpl
 import com.teambeme.beme.login.view.LoginActivity
@@ -22,19 +33,51 @@ import com.teambeme.beme.main.view.MainActivity
 import com.teambeme.beme.onboarding.view.OnBoardingActivity
 import com.teambeme.beme.util.ErrorBody
 import com.teambeme.beme.util.StatusBarUtil
+import kotlinx.coroutines.delay
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     private val loginRepository =
         LoginRepositoryImpl(LoginDataSourceImpl(RetrofitObjects.getLoginService()))
-
+    private val appUpdateManager by lazy() { AppUpdateManagerFactory.create(this) }
+    private val REQUEST_CODE_UPDATE = 9999
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
         StatusBarUtil.setStatusBar(this, R.color.black)
-        val handler = Handler(Looper.getMainLooper())
+        initInAppUpdate(true)
+        setFullScreen()
+        lifecycleScope.launchWhenCreated {
+            delay(1000L)
+            if (BeMeAuthPreference.isFirst) {
+                navigateToOnBoarding()
+            } else {
+                setNextActivity()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        appUpdateManager.appUpdateInfo.addOnSuccessListener {
+            if (it.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                runCatching {
+                    appUpdateManager.startUpdateFlowForResult(
+                        it,
+                        AppUpdateType.IMMEDIATE, // or AppUpdateType.IMMEDIATE
+                        this,
+                        REQUEST_CODE_UPDATE
+                    )
+                }.onFailure {
+                    it.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun setFullScreen() {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false)
             window.insetsController?.let {
@@ -50,20 +93,6 @@ class SplashActivity : AppCompatActivity() {
                     or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
         }
-        val thread = object : Thread() {
-            override fun run() {
-                runOnUiThread {
-                    handler.postDelayed({
-                        if (BeMeAuthPreference.isFirst) {
-                            navigateToOnBoarding()
-                        } else {
-                            setNextActivity()
-                        }
-                    }, 1000)
-                }
-            }
-        }
-        thread.start()
     }
 
     private fun navigateToOnBoarding() {
@@ -120,5 +149,76 @@ class SplashActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    private fun initInAppUpdate(inAppUpdateAvailable: Boolean) {
+        if (inAppUpdateAvailable) {
+            val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+            appUpdateManager.registerListener(object : InstallStateUpdatedListener {
+                override fun onStateUpdate(state: InstallState) {
+                    when (state.installStatus()) {
+                        InstallStatus.DOWNLOADED -> {
+                            popupSnackbarForCompleteUpdate()
+                        }
+                        InstallStatus.INSTALLED -> {
+                            appUpdateManager.unregisterListener(this)
+                        }
+                        else -> {
+                        }
+                    }
+                }
+            })
+
+            appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+                when (appUpdateInfo.updateAvailability()) {
+                    UpdateAvailability.UPDATE_AVAILABLE -> {
+                        appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+                        requestUpdate(appUpdateInfo)
+                    }
+                    else -> {
+                        Log.d("TAG", "Init UPDATE_AVAILABLE else")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun popupSnackbarForCompleteUpdate() {
+        Snackbar.make(
+            binding.root,
+            "업데이트가 완료되었습니다.",
+            Snackbar.LENGTH_INDEFINITE
+        ).apply {
+            setAction("RESTART") { appUpdateManager.completeUpdate() }
+            setActionTextColor(resources.getColor(Color.WHITE, null))
+            show()
+        }
+    }
+
+    private fun requestUpdate(appUpdateInfo: AppUpdateInfo) {
+        runCatching {
+            appUpdateManager.startUpdateFlowForResult(
+                appUpdateInfo,
+                AppUpdateType.IMMEDIATE, // or AppUpdateType.IMMEDIATE
+                this,
+                REQUEST_CODE_UPDATE
+            )
+        }.onFailure {
+            it.printStackTrace()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_CODE_UPDATE -> {
+                if (resultCode != Activity.RESULT_OK) {
+                    Toast.makeText(this, "업데이트가 취소되었습니다.", Toast.LENGTH_SHORT).show()
+                    finishAffinity()
+                }
+            }
+            else -> {
+            }
+        }
     }
 }
